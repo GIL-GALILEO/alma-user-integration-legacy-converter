4a3a83a77a8135f068e981a98ef552d83aee072f
Mike Kanning <mikekanning@gmail.com>
2016-09-09 18:58:10 -0400

    make mappings more readable

--- lib/objects/sif_user.rb
+++ lib/objects/sif_user.rb
@@ -3,22 +3,22 @@ require_relative 'user'
 class SifUser < User
 
   MAPPING = {
-          primary_id: [31,41],
-          first_name: [73,92],
-          middle_name: [93,112],
-          last_name: [43,72],
-          gender: nil,
-          user_group: [0,9],
-          campus_code: nil,
-          status: nil,
-          address_line_1: [141,190],
-          address_line_2: [191,240],
-          address_city: [271,310],
-          address_state_province: [311,317],
-          address_postal_code: [318,327],
-          address_country: [328,347],
-          email: [469,518],
-          phone: [348,372],
+          primary_id:               [31, 41],
+          first_name:               [73, 92],
+          middle_name:              [93, 112],
+          last_name:                [43, 72],
+          gender:                   nil,
+          user_group:               [0, 9],
+          campus_code:              nil,
+          status:                   nil,
+          address_line_1:           [141, 190],
+          address_line_2:           [191, 240],
+          address_city:             [271, 310],
+          address_state_province:   [311, 317],
+          address_postal_code:      [318, 327],
+          address_country:          [328, 347],
+          email:                    [469, 518],
+          phone:                    [348, 372],
   }
 
   def initialize(line_data)
--- lib/objects/txt_user.rb
+++ lib/objects/txt_user.rb
@@ -5,20 +5,20 @@ class TxtUser < User
   attr_accessor :name
 
   MAPPING = {
-      primary_id: 0,
-      name: 1,
-      gender: nil,
-      user_group: 2,
-      campus_code: nil,
-      status: nil,
-      address_line_1: 3,
-      address_line_2: nil,
-      address_city: 4,
-      address_state_province: 5,
-      address_postal_code: 6,
-      address_country: nil,
-      email: 14,
-      phone: 7,
+      primary_id:               0,
+      name:                     1,
+      gender:                   nil,
+      user_group:               2,
+      campus_code:              nil,
+      status:                   nil,
+      address_line_1:           3,
+      address_line_2:           nil,
+      address_city:             4,
+      address_state_province:   5,
+      address_postal_code:      6,
+      address_country:          nil,
+      email:                    14,
+      phone:                    7,
   }
 
   def initialize(line_data)
