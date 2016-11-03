bff62937c1a2293979ee649d12016aa1907249b7
Mike Kanning <mikekanning@gmail.com>
2016-11-03 13:47:43 -0400

    total refactor of SifUser class to better parse ZORVLIB output, and include multiple address types

--- lib/objects/sif_user.rb
+++ lib/objects/sif_user.rb
@@ -1,33 +1,115 @@
 require_relative 'user'
 
+# Parser object for standard ZORVLIB Process
+
 class SifUser < User
 
-  # todo: distinguish between a value where we want to use a default string value, and one we want to leave blank/nil
-  MAPPING = {
-          primary_id:               [31, 41],
-          first_name:               [73, 92],
-          middle_name:              [93, 112],
-          last_name:                [43, 72],
-          gender:                   nil,
-          user_group:               [0, 9],
-          campus_code:              nil,
-          status:                   nil,
-          address_line_1:           [141, 190],
-          address_line_2:           [191, 240],
-          address_city:             [271, 310],
-          address_state_province:   [311, 317],
-          address_postal_code:      [318, 327],
-          address_country:          [328, 347],
-          email:                    [469, 518],
-          phone:                    [348, 372],
-          secondary_id:             nil
+  USER_SEGMENT_LENGTH       = 120
+  ADDRESS_SEGMENT_LENGTH    = 328
+  MAXIMUM_ADDRESS_SEGMENTS  = 9
+
+  ADDRESS_TYPE_MAPPING = {
+      '1': 'Permanent Address',
+      '2': 'Temporary Address',
+      '3': 'Email Address'
+  }
+
+  GENERAL_MAPPING = {
+      user_group:          [0, 9],
+      # registration_date:   [10, 19],
+      primary_id:          [31, 41],
+      last_name:           [43, 72],
+      first_name:          [73, 92],
+      middle_name:         [93, 112],
+  }
+
+  ADDRESS_SEGMENT_MAPPING = {
+      address_type:             [0, 1],
+      address_line_1:           [21, 70],
+      address_line_2:           [71, 110],
+      address_city:             [151, 190],
+      address_state_province:   [191, 197],
+      address_postal_code:      [198, 207],
+      address_country:          [208, 227],
+      primary_phone:            [228, 252],
+      mobile_phone:             [253, 277],
   }
 
   def initialize(line_data, institution)
     @institution = institution
-    MAPPING.each do |attr, width|
-      self.send("#{attr}=", width ? line_data[width[0]..width[1]].strip : 'DEFAULT VALUE')
+    @line_data = line_data
+
+    user_data = extract_user_data
+    address_data = extract_addresses
+
+    user_data.each do |attr, value|
+        set_value attr, value
     end
+
+    address_data.each do |segment, data_hash|
+
+      case segment
+        when '1'
+          # permanent
+          set_primary_address(data_hash)
+        when '2'
+          # temporary
+          set_secondary_address(data_hash)
+        when '3'
+          # email
+          set_email_address(data_hash)
+        else
+          # discard?
+      end
+
+    end
+
+  end
+
+  def extract_addresses
+    address_data = {}
+    1...MAXIMUM_ADDRESS_SEGMENTS.each do |segment_num|
+      address_data[segment_num.to_s] = extract_address_for_segment segment_num
+    end
+    address_data
+  end
+
+  def extract_address_for_segment(segment_num)
+    segment_start = USER_SEGMENT_LENGTH + ( (segment_num - 1) * ADDRESS_SEGMENT_LENGTH )
+    segment_finish = segment_start + ADDRESS_SEGMENT_LENGTH
+    address_segment = @line_data[segment_start..segment_finish]
+    address_data = {}
+    ADDRESS_SEGMENT_MAPPING.each do |attr, width|
+      address_data[attr] = @line_data[width[0]..width[1]].strip
+    end
+    address_data
+  end
+
+  def extract_user_data
+    general_data = {}
+    GENERAL_MAPPING.each do |attr, width|
+      general_data[attr] = @line_data[width[0]..width[1]].strip
+    end
+  end
+
+  def set_primary_address(data_hash)
+    data_hash.each do |attr, value|
+      set_value "primary_#{attr}", value
+    end
+  end
+
+  def set_secondary_address(data_hash)
+    data_hash.each do |attr, value|
+      set_value "secondary_#{attr}", value
+    end
+  end
+
+  def set_email_address(data_hash)
+    set_value 'email', data_hash['address_line_1']
+  end
+
+  def set_value(attribute, value)
+    self.send("#{attribute}=", value)
   end
 
 end
