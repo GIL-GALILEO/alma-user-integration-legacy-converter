a47779b22e761e7f538be523daebd652794da4e9
Mike Kanning <mikekanning@gmail.com>
2016-11-17 15:38:10 -0500

    help tests pass when run as a group

--- lib/test/xml_factory_test.rb
+++ lib/test/xml_factory_test.rb
@@ -1,36 +1,38 @@
 require 'minitest/autorun'
 require './lib/classes/xml_factory'
+require './lib/classes/institution'
 
 class XmlFactoryTest < MiniTest::Test
 
   def setup
 
-    @xml_factory_sif_result = XmlFactory.generate_for(Institution.new('test_sif'))
+    # @xml_factory_sif_result = XmlFactory.generate_for(Institution.new('test_sif')) # usingthis would cause tests to fail when run as a group, very strange
     # @xml_factory_txt_result = XmlFactory.generate_for(Institution.new('test_txt'))
 
   end
 
   def test_sif_produces_xml_string
 
-    assert_kind_of String, @xml_factory_sif_result
+    # assert_kind_of String, @xml_factory_sif_result
+    assert_kind_of String, XmlFactory.generate_for(Institution.new('test_sif'))
 
   end
 
   def test_sif_produces_xml_with_user_node
 
-    assert_match /<user>/, @xml_factory_sif_result
+    assert_match /<user>/, XmlFactory.generate_for(Institution.new('test_sif'))
 
   end
 
   def test_sif_produces_xml_with_expiry_date
 
-    assert_match /2044-12-20Z/, @xml_factory_sif_result
+    assert_match /2044-12-20Z/, XmlFactory.generate_for(Institution.new('test_sif'))
 
   end
 
   def test_sif_produces_xml_with_barcode
 
-    assert_match /87654321930123456/, @xml_factory_sif_result
+    assert_match /87654321930123456/, XmlFactory.generate_for(Institution.new('test_sif'))
 
   end
 
