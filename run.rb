1fd1bdf168da7d35d3b1e94885ec6982ab556de4
Mike Kanning <mikekanning@gmail.com>
2016-12-02 10:05:26 -0500

    no secrets

--- run.rb
+++ run.rb
@@ -9,22 +9,12 @@ require './lib/util'
 include Util::App
 
 LOG_FILE     = './log.log'
-SECRETS_FILE = './config/secrets.yml'
 
 FILES_ROOT   = '/gilftpfiles'
 PICKUP_POINT = '/patrondrop'
 DROP_POINT   = '/sis/import'
 
 @script_logger = Logger.new LOG_FILE
-
-# Check for required files
-exit_log_error("Secrets file could not be found @ #{SECRETS_FILE}. Stopping.") unless File.exist? SECRETS_FILE
-
-# Load configs
-secrets = YAML.load_file SECRETS_FILE
-
-# Check configs
-exit_log_error('Secrets config file not properly parsed. Stopping.') unless secrets.is_a? Hash
 
 # Load params
 code = ARGV[0]
@@ -51,25 +41,6 @@ unless dry_run
   source_file = File.join Dir.pwd, zip_file.path
   destination_file = File.join FILES_ROOT, institution.code, DROP_POINT, File.basename(zip_file.path)
   FileUtils.mv(source_file, destination_file)
-
-  # FTP TO REMOTE SERVER
-  # remote_file = File.join(
-  #     secrets['ftp']['path'].gsub('__inst__', institution.code),
-  #     File.basename(zip_file)
-  # )
-  #
-  # begin
-  #   Net::SFTP.start(
-  #       secrets['ftp']['url'],
-  #       secrets['ftp']['user'],
-  #       password: secrets['ftp']['pass'],
-  #       port: secrets['ftp']['port']) do |c|
-  #     c.upload! zip_file, remote_file
-  #   end
-  #   @script_logger.info "File successfully delivered for #{institution.code}: #{zip_file}"
-  # rescue Exception => e
-  #   exit_log_error "Problem delivering file to GIL FTP server: #{e.message}"
-  # end
 
   # todo send email notifications here
 
