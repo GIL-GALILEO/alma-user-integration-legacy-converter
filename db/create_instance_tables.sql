1de1341e641ededa6ec468229b959031425e64f0
Mike Kanning <mikekanning@gmail.com>
2016-12-05 22:09:14 -0500

    tables for all institutions

---
+++ db/create_instance_tables.sql
@@ -0,0 +1,144 @@
+CREATE TABLE abac_patrons (
+  id     SERIAL PRIMARY KEY,
+  instID varchar(10)
+);
+
+CREATE TABLE armstrong_patrons (
+  id     SERIAL PRIMARY KEY,
+  instID varchar(10)
+);
+
+CREATE TABLE asu_patrons (
+  id     SERIAL PRIMARY KEY,
+  instID varchar(10)
+);
+
+CREATE TABLE atlm_patrons (
+  id     SERIAL PRIMARY KEY,
+  instID varchar(10)
+);
+
+CREATE TABLE augusta_patrons (
+  id     SERIAL PRIMARY KEY,
+  instID varchar(10)
+);
+
+CREATE TABLE bain_patrons (
+  id     SERIAL PRIMARY KEY,
+  instID varchar(10)
+);
+
+CREATE TABLE ccga_patrons (
+  id     SERIAL PRIMARY KEY,
+  instID varchar(10)
+);
+
+CREATE TABLE clayton_patrons (
+  id     SERIAL PRIMARY KEY,
+  instID varchar(10)
+);
+
+CREATE TABLE csu_patrons (
+  id     SERIAL PRIMARY KEY,
+  instID varchar(10)
+);
+
+CREATE TABLE dalton_patrons (
+  id     SERIAL PRIMARY KEY,
+  instID varchar(10)
+);
+
+CREATE TABLE darton_patrons (
+  id     SERIAL PRIMARY KEY,
+  instID varchar(10)
+);
+
+CREATE TABLE ega_patrons (
+  id     SERIAL PRIMARY KEY,
+  instID varchar(10)
+);
+
+CREATE TABLE ftv_patrons (
+  id     SERIAL PRIMARY KEY,
+  instID varchar(10)
+);
+
+CREATE TABLE gasou_patrons (
+  id     SERIAL PRIMARY KEY,
+  instID varchar(10)
+);
+
+CREATE TABLE gcsu_patrons (
+  id     SERIAL PRIMARY KEY,
+  instID varchar(10)
+);
+
+CREATE TABLE ggc_patrons (
+  id     SERIAL PRIMARY KEY,
+  instID varchar(10)
+);
+
+CREATE TABLE ghc_patrons (
+  id     SERIAL PRIMARY KEY,
+  instID varchar(10)
+);
+
+CREATE TABLE gordon_patrons (
+  id     SERIAL PRIMARY KEY,
+  instID varchar(10)
+);
+
+CREATE TABLE gptc_patrons (
+  id     SERIAL PRIMARY KEY,
+  instID varchar(10)
+);
+
+CREATE TABLE gsu_patrons (
+  id     SERIAL PRIMARY KEY,
+  instID varchar(10)
+);
+
+CREATE TABLE gsw_patrons (
+  id     SERIAL PRIMARY KEY,
+  instID varchar(10)
+);
+
+CREATE TABLE ksu_patrons (
+  id     SERIAL PRIMARY KEY,
+  instID varchar(10)
+);
+
+CREATE TABLE mga_patrons (
+  id     SERIAL PRIMARY KEY,
+  instID varchar(10)
+);
+
+CREATE TABLE savst_patrons (
+  id     SERIAL PRIMARY KEY,
+  instID varchar(10)
+);
+
+CREATE TABLE sgsc_patrons (
+  id     SERIAL PRIMARY KEY,
+  instID varchar(10)
+);
+
+CREATE TABLE uga_patrons (
+  id     SERIAL PRIMARY KEY,
+  instID varchar(10)
+);
+
+CREATE TABLE ung_patrons (
+  id     SERIAL PRIMARY KEY,
+  instID varchar(10)
+);
+
+CREATE TABLE vsu_patrons (
+  id     SERIAL PRIMARY KEY,
+  instID varchar(10)
+);
+
+CREATE TABLE westga_patrons (
+  id     SERIAL PRIMARY KEY,
+  instID varchar(10)
+);
