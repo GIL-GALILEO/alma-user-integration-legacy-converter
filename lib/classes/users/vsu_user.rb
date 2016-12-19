0678f5c6da4fede3c351c80523e37901cedd163b
Mike Kanning <mikekanning@gmail.com>
2016-12-19 14:35:10 -0500

    finish vsu user class and tests

---
+++ lib/classes/users/vsu_user.rb
@@ -0,0 +1,49 @@
+require './lib/classes/users/sif_user'
+
+class WestGaUser < SifUser
+
+  USER_SEGMENT_LENGTH = 488
+  ADDRESS_SEGMENT_LENGTH = 429
+  MAXIMUM_ADDRESS_SEGMENTS = 1
+
+  GENERAL_MAPPING = {
+      user_group: [45, 55],
+      first_name: [340, 360],
+      middle_name: [360, 380],
+      last_name: [310, 330],
+      email: [917, 967],
+      barcode: [20, 29],
+      primary_id: [238, 248],
+  }
+
+  ADDRESS_SEGMENT_MAPPING = {
+      address_line_1:           [0, 99],
+      address_line_2:           [99, 199],
+      address_city:             [210, 250],
+      address_state_province:   [250, 257],
+      address_postal_code:      [257, 267],
+      address_country:          [267, 287],
+      address_phone:            [287, 307],
+  }
+
+  def general_mapping
+    GENERAL_MAPPING
+  end
+
+  def user_segment_length
+    USER_SEGMENT_LENGTH
+  end
+
+  def address_segment_length
+    ADDRESS_SEGMENT_LENGTH
+  end
+
+  def maximum_address_segments
+    MAXIMUM_ADDRESS_SEGMENTS
+  end
+
+  def address_segment_mapping
+    ADDRESS_SEGMENT_MAPPING
+  end
+
+end
---
+++ lib/test/users/vsu_user_test.rb
@@ -0,0 +1,108 @@
+require 'minitest/autorun'
+require 'yaml'
+require './lib/classes/users/vsu_user'
+require './lib/classes/institution'
+
+class VsuUserTest < MiniTest::Test
+
+  TEST_DATA_FILE = './config/test_data.yml'
+
+  def setup
+
+    test_data = YAML.load_file TEST_DATA_FILE
+    test_inst = Institution.new 'test_vsu'
+    @user = WestGaUser.new test_data['vsu_test'], test_inst
+
+  end
+
+  def test_has_user_group
+
+    assert_equal 'STUDNTGRA', @user.user_group
+
+  end
+
+  def test_has_primary_id
+
+    assert_equal '870123456', @user.primary_id
+
+  end
+
+  def test_has_barcode
+
+    assert_equal '870123456', @user.barcode
+
+  end
+
+  def test_has_first_name
+
+    assert_equal 'Ronald', @user.first_name
+
+  end
+
+  def test_has_middle_name
+
+    assert_equal 'Fry', @user.middle_name
+
+  end
+
+  def test_has_last_name
+
+    assert_equal 'McDonald', @user.last_name
+
+  end
+
+  def test_has_primary_address_line_1
+
+    assert_equal 'Rt 1 Box 100', @user.primary_address_line_1
+
+  end
+
+  def test_has_primary_address_line_2
+
+    assert_equal '', @user.primary_address_line_2
+
+  end
+
+  def test_has_primary_address_city
+
+    assert_equal 'Lakeland', @user.primary_address_city
+
+  end
+
+  def test_has_primary_address_postal_code
+
+    assert_equal '31635', @user.primary_address_postal_code
+
+  end
+
+  def test_has_primary_address_state_province
+
+    assert_equal 'GA', @user.primary_address_state_province
+
+  end
+
+  def test_has_primary_address_country
+
+    assert_equal '', @user.primary_address_country
+
+  end
+
+  def test_has_primary_address_phone
+
+    assert_equal '222-333-4444', @user.primary_address_phone
+
+  end
+
+  def test_has_primary_address_mobile_phone
+
+    assert_equal nil, @user.primary_address_mobile_phone
+
+  end
+
+  def test_has_email
+
+    assert_equal 'frmcdonald@valdosta.edu', @user.email
+
+  end
+
+end
