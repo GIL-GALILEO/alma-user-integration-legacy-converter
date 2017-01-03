8ad92a628775570be8dd7b8c0f0cd072c34817b1
Mike Kanning <mikekanning@gmail.com>
2017-01-03 14:44:04 -0500

    kennesaw user class and test

---
+++ lib/classes/users/ksu_user.rb
@@ -0,0 +1,49 @@
+require './lib/classes/users/sif_user'
+
+class KsuUser < SifUser
+
+  KSU_USER_SEGMENT_LENGTH = 466
+  KSU_ADDRESS_SEGMENT_LENGTH = 429
+  KSU_MAXIMUM_ADDRESS_SEGMENTS = 2
+
+  KSU_GENERAL_MAPPING = {
+      barcode:      [20, 40],
+      user_group:   [45, 55],
+      first_name:   [340, 360],
+      middle_name:  [360, 380],
+      last_name:    [310, 330],
+      primary_id:   [238, 248],
+  }
+
+  KSU_ADDRESS_SEGMENT_MAPPING = {
+      address_type:             [0, 1],
+      address_line_1:           [22, 112],
+      address_line_2:           [112, 232],
+      address_city:             [232, 272],
+      address_state_province:   [272, 279],
+      address_postal_code:      [279, 289],
+      address_country:          [289, 309],
+      address_phone:            [309, 321],
+  }
+
+  def general_mapping
+    KSU_GENERAL_MAPPING
+  end
+
+  def user_segment_length
+    KSU_USER_SEGMENT_LENGTH
+  end
+
+  def address_segment_length
+    KSU_ADDRESS_SEGMENT_LENGTH
+  end
+
+  def maximum_address_segments
+    KSU_MAXIMUM_ADDRESS_SEGMENTS
+  end
+
+  def address_segment_mapping
+    KSU_ADDRESS_SEGMENT_MAPPING
+  end
+
+end
---
+++ lib/test/users/ksu_user_test.rb
@@ -0,0 +1,108 @@
+require 'minitest/autorun'
+require 'yaml'
+require './lib/classes/users/ksu_user'
+require './lib/classes/institution'
+
+class KsuUserTest < MiniTest::Test
+
+  TEST_DATA_FILE = './config/test_data.yml'
+
+  def setup
+
+    test_data = YAML.load_file TEST_DATA_FILE
+    test_inst = Institution.new 'test_ksu'
+    @user = KsuUser.new test_data['ksu_test'], test_inst
+
+  end
+
+  def test_has_user_group
+
+    assert_equal 'f-fac/staf', @user.user_group
+
+  end
+
+  def test_has_primary_id
+
+    assert_equal '000123456', @user.primary_id
+
+  end
+
+  def test_has_barcode
+
+    assert_equal '12345678900000', @user.barcode
+
+  end
+
+  def test_has_first_name
+
+    assert_equal 'Robert', @user.first_name
+
+  end
+
+  def test_has_middle_name
+
+    assert_equal 'F', @user.middle_name
+
+  end
+
+  def test_has_last_name
+
+    assert_equal 'Gates', @user.last_name
+
+  end
+
+  def test_has_primary_address_line_1
+
+    assert_equal '1234 Renaissance St NW', @user.primary_address_line_1
+
+  end
+
+  def test_has_primary_address_line_2
+
+    assert_equal 'Some Midtown Apartments', @user.primary_address_line_2
+
+  end
+
+  def test_has_primary_address_city
+
+    assert_equal 'Atlanta', @user.primary_address_city
+
+  end
+
+  def test_has_primary_address_postal_code
+
+    assert_equal '30000', @user.primary_address_postal_code
+
+  end
+
+  def test_has_primary_address_state_province
+
+    assert_equal 'GA', @user.primary_address_state_province
+
+  end
+
+  def test_has_primary_address_country
+
+    assert_equal '..', @user.primary_address_country
+
+  end
+
+  def test_has_primary_address_phone
+
+    assert_equal '', @user.primary_address_phone
+
+  end
+
+  def test_has_primary_address_mobile_phone
+
+    assert_nil @user.primary_address_mobile_phone
+
+  end
+
+  def test_has_email
+
+    assert_equal 'settingw@kennesaw.edu', @user.email
+
+  end
+
+end
