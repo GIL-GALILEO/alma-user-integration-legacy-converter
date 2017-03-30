041caf89298b28340f8018bb045f14316c4f0023
Mike Kanning <mikekanning@gmail.com>
2017-03-30 14:31:25 -0400

    add todo and address RuboCop violations in Slacker

--- lib/classes/slacker.rb
+++ lib/classes/slacker.rb
@@ -1,12 +1,14 @@
 require 'slack'
 require 'yaml'
-# require './lib/classes/institution'
-# require './lib/classes/user'
 
+#
+# Class for handling connecting to and sending messages to Slack
+# TODO: Fail silently if no Net or Slack down
+#
 class Slacker
 
-  SECRETS_FILE = './config/secrets.yml'
-  CHANNEL = 'alma-patron-loads'
+  SECRETS_FILE = './config/secrets.yml'.freeze
+  CHANNEL = 'alma-patron-loads'.freeze
 
   def initialize(institution)
 
@@ -22,11 +24,9 @@ class Slacker
 
   def post(text, options = {})
 
-    options.merge!({
-        text: text,
-        channel: CHANNEL,
-        as_user: true
-                   })
+    options[:text] = text
+    options[:channel] = CHANNEL
+    options[:as_user] = true
 
     Slack.chat_postMessage(options) unless defined?(MiniTest)
 
@@ -34,19 +34,19 @@ class Slacker
 
   def users_extracted(users, errors)
     post(
-        "Patron file from `#{@inst.code}` parsed. #{users} patrons extracted with #{errors} errors."
+      "Patron file from `#{@inst.code}` parsed. #{users} patrons extracted with #{errors} errors."
     )
   end
 
   def critical_error(text)
     post(
-        "Job Failed for `#{@inst.code}`. Error message:\n#{text}"
+      "Job Failed for `#{@inst.code}`. Error message:\n#{text}"
     )
   end
 
   def run_completed
     post(
-        "Job Completed for `#{@inst.code}`."
+      "Job Completed for `#{@inst.code}`."
     )
   end
 
