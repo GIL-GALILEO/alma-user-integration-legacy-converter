074334dbd86d3dac4f8f07d13630636d88feb839
Mike Kanning <mikekanning@gmail.com>
2017-04-26 15:47:56 -0400

    address Rubocop warnings and silly exception handling in Zipper

--- lib/classes/zipper.rb
+++ lib/classes/zipper.rb
@@ -2,36 +2,23 @@ require 'fileutils'
 require 'zip'
 
 class Zipper
-
-  ZIP_ARCHIVE_PATH = '/gilftpfiles/__INST_CODE__/archive/'
+  ZIP_ARCHIVE_PATH = '/gilftpfiles/__INST_CODE__/archive/'.freeze
 
   def self.do(file, institution)
-
     files_path = ZIP_ARCHIVE_PATH.gsub('__INST_CODE__', institution.code)
-
-    FileUtils::mkpath files_path
-
+    FileUtils.mkpath files_path
     file_basename = File.basename file
     xml_file_path = File.join files_path, file_basename
     zip_file_name = file_basename.gsub '.xml', '.zip'
     zip_file_path = File.join files_path, zip_file_name
-
     begin
-
       Zip::File.open(zip_file_path, Zip::File::CREATE) do |zipfile|
         zipfile.add file_basename, File.realpath(file)
       end
-
-    rescue Exception => e
-
-      raise StandardError.new("Problem compressing XML file for delivery: #{e.message}")
-
+    rescue StandardError => e
+      exit_log_error "Problem compressing XML file for delivery: #{e.message}"
     end
-
     FileUtils.mv(File.realpath(file), xml_file_path)
-
     File.new zip_file_path
-
   end
-
 end
