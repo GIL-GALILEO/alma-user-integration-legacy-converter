9d0e8cf22fae163d040543e5ea53abc47065a20f
Mike Kanning <mikekanning@gmail.com>
2017-06-06 08:14:54 -0400

    avoid usage of 'temp' folder

--- lib/classes/templater.rb
+++ lib/classes/templater.rb
@@ -8,19 +8,19 @@ include Util::File
 
 class Templater
 
-  XML_TEMPLATE_FILE = './lib/templates/user_xml_v2_template.xml.erb'
-  DEFAULTS_FILE = './config/defaults.yml'
+  XML_TEMPLATE_FILE = './lib/templates/user_xml_v2_template.xml.erb'.freeze
+  DEFAULTS_FILE = './config/defaults.yml'.freeze
 
   def self.run(users, run_set)
 
     institution = run_set.inst
 
-    raise StandardError.new("Could not find XML template file @ #{XML_TEMPLATE_FILE}. Stopping.") unless File.exist? XML_TEMPLATE_FILE
-    raise StandardError.new("Defaults file could not be found @ #{DEFAULTS_FILE}. Stopping.") unless File.exist? DEFAULTS_FILE
+    fail(StandardError, "Could not find XML template file @ #{XML_TEMPLATE_FILE}. Stopping.") unless File.exist? XML_TEMPLATE_FILE
+    fail(StandardError, "Defaults file could not be found @ #{DEFAULTS_FILE}. Stopping.") unless File.exist? DEFAULTS_FILE
 
     defaults = YAML.load_file DEFAULTS_FILE
 
-    raise StandardError.new('Defaults config file not properly parsed. Stopping.') unless defaults.is_a? Hash
+    fail(StandardError, 'Defaults config file not properly parsed. Stopping.') unless defaults.is_a? Hash
 
     template = File.open XML_TEMPLATE_FILE
     defaults = OpenStruct.new defaults['global']
@@ -28,14 +28,10 @@ class Templater
     # Read template
     template = ERB.new(template.read)
 
-    if run_set.config.has_key?(:run_type) && run_set.config[:run_type] == :expire
-      output_filename = "./temp/#{institution.code}_expire_patrons_#{Time.now.strftime('%Y%m%d')}.xml"
-    else
-      output_filename = "./temp/#{institution.code}_patrons_#{Time.now.strftime('%Y%m%d')}.xml"
+    output_filename = "#{institution.code}_patrons_#{Time.now.strftime('%Y%m%d')}.xml"
+    output_filepath = File.join(institution.alma_archive_path, output_filename)
 
-    end
-
-    file = File.open(output_filename, 'w')
+    file = File.open(output_filepath, 'w')
 
     # Initialize XML
     file.puts "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<users>"
@@ -47,8 +43,8 @@ class Templater
         user.order_phone_numbers
         xml = template.result(binding).gsub(/\n\s*\n/, "\n")
         file.puts xml
-      rescue Exception => e
-        msg = "Error creating XML for User on row #{row}: #{e.message}"
+      rescue StandardError => te
+        msg = "Error creating XML for User on row #{row}: #{te.message}"
         institution.logger.error msg
         institution.mailer.add_script_error_message msg
       ensure
--- lib/classes/zipper.rb
+++ lib/classes/zipper.rb
@@ -8,7 +8,6 @@ class Zipper
     files_path = ZIP_ARCHIVE_PATH.gsub('__INST_CODE__', institution.code)
     FileUtils.mkpath files_path
     file_basename = File.basename file
-    xml_file_path = File.join files_path, file_basename
     zip_file_name = file_basename.gsub '.xml', '.zip'
     zip_file_path = File.join files_path, zip_file_name
     begin
@@ -16,9 +15,9 @@ class Zipper
         zipfile.add file_basename, File.realpath(file)
       end
     rescue StandardError => e
+      # TODO cleanup failed run
       exit_log_error "Problem compressing XML file for delivery: #{e.message}"
     end
-    FileUtils.mv(File.realpath(file), xml_file_path)
     File.new zip_file_path
   end
 end
