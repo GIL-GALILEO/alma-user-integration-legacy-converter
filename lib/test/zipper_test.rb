e07e7b28a3c2da6b9125964a332ef260aa2dd06f
Mike Kanning <mikekanning@gmail.com>
2017-06-06 16:31:46 -0400

    address rubocop issues in zipper test

--- lib/test/zipper_test.rb
+++ lib/test/zipper_test.rb
@@ -5,28 +5,20 @@ require './lib/classes/institution'
 class ZipperTest < MiniTest::Test
 
   def setup
-
-    @xml_file = File.open('./temp/text.xml','w')
+    @xml_file = File.open('./temp/text.xml', 'w')
     @xml_file.puts 'test content'
     @xml_file.close
-
     @institution = Institution.new 'test_sif'
-
     @file = Zipper.do(@xml_file, @institution)
-
   end
 
   def test_file_is_returned
-
     assert_kind_of File, @file
-
   end
 
   def teardown
-
     File.delete(File.realpath(@file).gsub!('.zip','.xml'))
     File.delete @file
-
   end
 
 end
