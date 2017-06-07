bdb542dd72bcb5173bb0fcb3a7b604652ae8595d
Mike Kanning <mikekanning@gmail.com>
2017-06-07 17:26:29 -0400

    show mtimes for discovered files in check.rb

--- check.rb
+++ check.rb
@@ -13,18 +13,19 @@ inst_configs.each do |institution|
     run_set = FileHandler.new(institution).run_set
     run_set.file_sets.each do |file_set|
       if file_set.patrons.length > 0
+        times = file_set.patrons.map { |f| File.new(f).mtime }.join("\n")
         message = if file_set.campus
-                    "`#{institution.code}` `#{file_set.campus.path}` has an unprocessed file"
+                    "`#{institution.code}` `#{file_set.campus.path}` has an unprocessed file. mtimes: ```#{times}```"
                   else
-                    "`#{institution.code}` has an unprocessed file"
+                    "`#{institution.code}` has an unprocessed file. mtimes: ```#{times}```"
                   end
-        institution.slacker.post(message) unless defined?(MiniTest)
+        # institution.slacker.post(message) unless defined?(MiniTest)
         puts message unless defined?(MiniTest)
       end
     end
   rescue StandardError => e
     error_message = "problem with `#{institution.code}`: ```#{e}```"
-    institution.slacker.post(error_message) unless defined?(MiniTest)
+    # institution.slacker.post(error_message) unless defined?(MiniTest)
     puts error_message unless defined?(MiniTest)
   end
 end
