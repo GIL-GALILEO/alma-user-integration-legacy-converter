0636d813832e27d28ed2629f56e92b81df67a821
Mike Kanning <mikekanning@gmail.com>
2020-01-13 14:51:35 -0500

    add comments for UGA mapping

--- lib/classes/users/uga_user.rb
+++ lib/classes/users/uga_user.rb
@@ -14,24 +14,29 @@ class UgaUser < User
   LAST_PAY_EXPIRE_DAYS = 60
   NEVER_EXPIRE_FS_CODES = %w(04 08).freeze
   MAPPING = {
-    primary_id:                       0,
-    name:                             1,
-    fs_codes:                         2,
-    primary_address_line_1:           3,
-    primary_address_city:             4,
-    primary_address_state_province:   5,
-    primary_address_postal_code:      6,
-    primary_address_phone:            7,
-    secondary_address_line_1:         8,
-    secondary_address_city:           9,
-    secondary_address_state_province: 10,
-    secondary_address_postal_code:    11,
-    secondary_address_phone:          13,
-    email:                            15,
-    class_code:                       16,
-    last_enrolled_date:               19,
-    last_pay_date:                    21,
-    barcode:                          22
+    primary_id:                       0,  # CAN
+    name:                             1,  # NAME
+    fs_codes:                         2,  # FSCODES
+    primary_address_line_1:           3,  # LA
+    primary_address_city:             4,  # LC
+    primary_address_state_province:   5,  # LS
+    primary_address_postal_code:      6,  # LZ
+    primary_address_phone:            7,  # LP
+    secondary_address_line_1:         8,  # PA
+    secondary_address_city:           9,  # PC
+    secondary_address_state_province: 10, # PS
+    secondary_address_postal_code:    11, # PZ
+                                          # PCT
+    secondary_address_phone:          13, # PP
+                                          # RA
+    email:                            15, # EMAIL
+    class_code:                       16, # CLASS
+                                          # SCHLN
+                                          # MAJ
+    last_enrolled_date:               19, # LTA
+                                          # DPTN
+    last_pay_date:                    21, # RU
+    barcode:                          22  # LIBBAR
   }.freeze
 
   def initialize(line_data, institution, _campus = nil)
